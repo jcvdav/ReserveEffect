ceae0c8110a7880ce895dd73930556cfcc85f8a7
jcvdav <juancarlos.villader@gmail.com>
2017-09-25 14:05:15 -0700

    Ajuste para incluir especies objetivo

--- extract_effect.R
+++ extract_effect.R
@@ -1,7 +1,14 @@
 extract_effect <- function(x){
   
-  x$Modelo %>%
+  extraction <- filter(x, !is.na(Modelo)) %$%
+    Modelo %>% 
     map_df(robust_se) %>% 
-    filter(term == "ZonaReserva:Post") %>% 
-    mutate(Indicador = c("ShannonP", "RiquezaP", "DensidadP", "TroficoP", "BiomasaP", "ShannonI", "RiquezaI", "DensidadI"))
+    filter(term == "ZonaReserva:Post")
+  
+  Indicador = c("ShannonP", "RiquezaP", "DensidadP", "TroficoP", "BiomasaP", "ShannonI", "RiquezaI", "DensidadI", "DensidadObj1", "DensidadObj2", "DensidadObj3", "DensidadObj4", "DensidadObj5", "DensidadObj6", "DensidadObj7")
+  
+  extraction$Indicador <- Indicador[1:dim(extraction)[1]]
+  
+  return(extraction)
+    
 }
