19b5bb9344886f0542c076dff586f228a0245d75
jcvdav <juancarlos.villader@gmail.com>
2017-09-25 14:04:58 -0700

    Agregue especies objetivo

--- analisis.R
+++ analisis.R
@@ -1,8 +1,6 @@
-analisis <- function(peces, invert, comunidad, ano.imp){
+analisis <- function(peces, invert, comunidad, ano.imp, obj.spp = ""){
   
-  # "Índice de diversidad de shannon", "Riqueza", "Organismos > LT50", "Densidad", "Densidad de especies objetivo", "Nivel trófico", "Biomasa", "Biomasa de especies objetivo"
-  
-  results <- tibble(Indicador = c("ShannonP", "RiquezaP", "DensidadP", "TroficoP", "BiomasaP", "ShannonI", "RiquezaI", "DensidadI"),
+  results <- tibble(Indicador = c("ShannonP", "RiquezaP", "DensidadP", "TroficoP", "BiomasaP", "ShannonI", "RiquezaI", "DensidadI", "DensidadObj1", "DensidadObj2", "DensidadObj3", "DensidadObj4", "DensidadObj5", "DensidadObj6", "DensidadObj7"),
                     Modelo = list(NA))
   
   results$Modelo[[1]] <- shannon(data = peces, location = comunidad) %>% 
@@ -30,20 +28,101 @@ analisis <- function(peces, invert, comunidad, ano.imp){
            Ano = as.factor(Ano)) %>% 
     modelo()
   
-  results$Modelo[[6]] <- shannon(data = peces, location = comunidad) %>% 
+  results$Modelo[[6]] <- shannon(data = invert, location = comunidad) %>% 
     mutate(Post = ifelse(Ano <= ano.imp, 0, 1),
            Ano = as.factor(Ano)) %>% 
     modelo()
   
-  results$Modelo[[7]] <- richness(data = peces, location = comunidad) %>% 
+  results$Modelo[[7]] <- richness(data = invert, location = comunidad) %>% 
     mutate(Post = ifelse(Ano <= ano.imp, 0, 1),
            Ano = as.factor(Ano)) %>% 
     modelo()
   
-  results$Modelo[[8]] <- density(data = peces, location = comunidad) %>% 
+  results$Modelo[[8]] <- density(data = invert, location = comunidad) %>% 
     mutate(Post = ifelse(Ano <= ano.imp, 0, 1),
            Ano = as.factor(Ano)) %>% 
     modelo()
+  
+  ## Especies objetivo
+  
+  ### Densidades
+  
+  if(length(obj.spp) > 1){
+    n <-  1
+    if(obj.spp[n] %in% unique(peces$GeneroEspecie)){data <- peces}
+    if(obj.spp[n] %in% unique(invert$GeneroEspecie)){data <- invert}
+    
+    results$Modelo[[n+8]] <- density(data = data, location = comunidad, species = obj.spp[n]) %>% 
+      mutate(Post = ifelse(Ano <= ano.imp, 0, 1),
+             Ano = as.factor(Ano)) %>% 
+      modelo()
+  }
+  
+  if(length(obj.spp) >= 2){
+    n <-  2
+    if(obj.spp[n] %in% unique(peces$GeneroEspecie)){data <- peces}
+    if(obj.spp[n] %in% unique(invert$GeneroEspecie)){data <- invert}
+    
+    results$Modelo[[n+8]] <- density(data = data, location = comunidad, species = obj.spp[n]) %>% 
+      mutate(Post = ifelse(Ano <= ano.imp, 0, 1),
+             Ano = as.factor(Ano)) %>% 
+      modelo()
+  }
+  
+  if(length(obj.spp) >= 3){
+    n <-  3
+    if(obj.spp[n] %in% unique(peces$GeneroEspecie)){data <- peces}
+    if(obj.spp[n] %in% unique(invert$GeneroEspecie)){data <- invert}
+    
+    results$Modelo[[n+8]] <- density(data = data, location = comunidad, species = obj.spp[n]) %>% 
+      mutate(Post = ifelse(Ano <= ano.imp, 0, 1),
+             Ano = as.factor(Ano)) %>% 
+      modelo()
+  }
+  
+  if(length(obj.spp) >= 4){
+    n <-  4
+    if(obj.spp[n] %in% unique(peces$GeneroEspecie)){data <- peces}
+    if(obj.spp[n] %in% unique(invert$GeneroEspecie)){data <- invert}
+    
+    results$Modelo[[n+8]] <- density(data = data, location = comunidad, species = obj.spp[n]) %>% 
+      mutate(Post = ifelse(Ano <= ano.imp, 0, 1),
+             Ano = as.factor(Ano)) %>% 
+      modelo()
+  }
+  
+  if(length(obj.spp) >= 5){
+    n <-  5
+    if(obj.spp[n] %in% unique(peces$GeneroEspecie)){data <- peces}
+    if(obj.spp[n] %in% unique(invert$GeneroEspecie)){data <- invert}
+    
+    results$Modelo[[n+8]] <- density(data = data, location = comunidad, species = obj.spp[n]) %>% 
+      mutate(Post = ifelse(Ano <= ano.imp, 0, 1),
+             Ano = as.factor(Ano)) %>% 
+      modelo()
+  }
+  
+  if(length(obj.spp) >= 6){
+    n <-  6
+    if(obj.spp[n] %in% unique(peces$GeneroEspecie)){data <- peces}
+    if(obj.spp[n] %in% unique(invert$GeneroEspecie)){data <- invert}
+    
+    results$Modelo[[n+8]] <- density(data = data, location = comunidad, species = obj.spp[n]) %>% 
+      mutate(Post = ifelse(Ano <= ano.imp, 0, 1),
+             Ano = as.factor(Ano)) %>% 
+      modelo()
+  }
+  
+  if(length(obj.spp) >= 7){
+    n <-  7
+    if(obj.spp[n] %in% unique(peces$GeneroEspecie)){data <- peces}
+    if(obj.spp[n] %in% unique(invert$GeneroEspecie)){data <- invert}
+    
+    results$Modelo[[n+8]] <- density(data = data, location = comunidad, species = obj.spp[n]) %>% 
+      mutate(Post = ifelse(Ano <= ano.imp, 0, 1),
+             Ano = as.factor(Ano)) %>% 
+      modelo()
+  }
   
   return(results)
 }
