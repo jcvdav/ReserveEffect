96ebece488a7320c446ee93cd4d7c41fb77db6c9
jcvdav <juancarlos.villader@gmail.com>
2017-09-25 14:05:40 -0700

    Cambios a tamano de puntos

--- my_ggplot.R
+++ my_ggplot.R
@@ -3,7 +3,7 @@ my_ggplot <- function(x, y.label){
   x$model %>% 
     mutate(Ano = as.numeric(as.character(Ano))) %>% 
     ggplot(aes(x = Ano, y = Indicador, color = Zona, fill = Zona)) +
-    geom_jitter(height = 0, width = 0.1) +
+    geom_jitter(height = 0, width = 0.1, size = 1, alpha = 0.5) +
     stat_summary(geom = "ribbon", fun.data = "mean_se", alpha = 0.5)+
     stat_summary(geom = "point", fun.data = "mean_se", color = "black")+
     stat_summary(geom = "line", fun.data = "mean_se", color = "black") +
