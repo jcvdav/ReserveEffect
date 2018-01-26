d3d3410ffd371bc9768ee1a5ab533e1053d922c1
jcvdav <juancarlos.villader@gmail.com>
2018-01-25 20:22:39 -0800

    Deleted old abundance_model fit

--- Scripts/abundance_model.R
+++
@@ -1,8 +0,0 @@
-abundance_model <- function(x){
-  x %>% 
-    mutate(Ano = as.factor(Ano)) %>% 
-    group_by(RC, Ano, Sitio, Zona, Transecto) %>% 
-    summarize(Abundancia = sum(Abundancia)) %>% 
-    ungroup() %>% 
-    glm(Abundancia ~ Ano * Zona, data = ., family = "quasipoisson")
-}
